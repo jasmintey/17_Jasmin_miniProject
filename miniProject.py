c3e1251453750bb73bb850fbaf074291ed5536ad
emilia-toh <65080653+emilia-toh@users.noreply.github.com>
2021-08-07 17:49:26 +0800

    hiiiii

--- miniProject.py
+++ miniProject.py
@@ -9,6 +9,17 @@ print(grp2)
 grp3 = file.iloc[21:]
 print(grp3)
 
-print(sum(grp1['Calories']))
-print(sum(grp2['Calories']))
-print(sum(grp3['Calories']))
+grp1sum = sum(grp1['Calories'])
+print(round(grp1sum, 1))
+grp2sum = sum(grp2['Calories'])
+print(grp2sum)
+grp3sum = sum(grp3['Calories'])
+print(grp3sum)
+
+grp1mean = grp1sum / len(grp1['Calories'])
+print(round(grp1mean, 2))
+grp2mean = grp2sum / len(grp2['Calories'])
+print(round(grp2mean, 2))
+grp3mean = grp3sum / len(grp3['Calories'])
+print(round(grp3mean, 2))
+
