e3d6c5de7edaebd73864fb9181ae4c8157009c07
emilia-toh <65080653+emilia-toh@users.noreply.github.com>
2021-08-07 17:10:19 +0800

    hiiiii

--- miniProject.py
+++ miniProject.py
@@ -2,4 +2,11 @@ import pandas as pd
 file = pd.read_excel('dataNew.xls')
 print(file)
 
+grp1 = file.iloc[:11]
+print(grp1)
+grp2 = file.iloc[11:21]
+print(grp2)
+grp3 = file.iloc[21:]
+print(grp3)
 
+print(sum(grp1['Calories']))
