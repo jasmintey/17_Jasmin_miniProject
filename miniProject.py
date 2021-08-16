d555b60b9baf0c87b6df8c8d95bdcc4888268ff8
jasmintey <65060477+jasmintey@users.noreply.github.com>
2021-08-16 15:35:33 +0800

    hiiiiii

--- miniProject.py
+++ miniProject.py
@@ -21,40 +21,40 @@ grp3 = file.iloc[21:] # Summarize the last 21 columns
 print(grp3)
 
 
-# grp1sum = sum(grp1['Calories'])
-# print(round(grp1sum, 1))
-# grp2sum = sum(grp2['Calories'])
-# print(grp2sum)
-# grp3sum = sum(grp3['Calories'])
-# print(grp3sum)
-#
-# grp1mean = grp1sum / len(grp1['Calories']) # Find the mean for the dataframe columns
-# print(round(grp1mean, 2))
-# grp2mean = grp2sum / len(grp2['Calories'])
-# print(round(grp2mean, 2))
-# grp3mean = grp3sum / len(grp3['Calories'])
-# print(round(grp3mean, 2))
-#
-# ps = grp1['Calories'].sort_values() # find the calories in the data file and present the result in bar chart in ascending order
-# plt.xlabel('Year', fontsize=10)
-# plt.ylabel('No. of calories', fontsize=10)
-# plt.title('1900-1910')
-# plt.bar(ps.index, ps.values)
-# plt.show()
-#
-# ps = grp2['Calories'].sort_values()
-# plt.xlabel('Year', fontsize=10)
-# plt.ylabel('No. of calories', fontsize=10)
-# plt.title('1911-1920')
-# plt.bar(ps.index, ps.values)
-# plt.show()
-#
-# ps = grp3['Calories'].sort_values()
-# plt.xlabel('Year', fontsize=10)
-# plt.ylabel('No. of calories', fontsize=10)
-# plt.title('1921-1930')
-# plt.bar(ps.index, ps.values)
-# plt.show()
+grp1sum = sum(grp1['Calories'])
+print(round(grp1sum, 1))
+grp2sum = sum(grp2['Calories'])
+print(grp2sum)
+grp3sum = sum(grp3['Calories'])
+print(grp3sum)
+
+grp1mean = grp1sum / len(grp1['Calories']) # Find the mean for the dataframe columns
+print(round(grp1mean, 2))
+grp2mean = grp2sum / len(grp2['Calories'])
+print(round(grp2mean, 2))
+grp3mean = grp3sum / len(grp3['Calories'])
+print(round(grp3mean, 2))
+
+ps = grp1['Calories'].sort_values() # find the calories in the data file and present the result in bar chart in ascending order
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.title('1900-1910')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+ps = grp2['Calories'].sort_values()
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.title('1911-1920')
+plt.bar(ps.index, ps.values)
+plt.show()
+
+ps = grp3['Calories'].sort_values()
+plt.xlabel('Year', fontsize=10)
+plt.ylabel('No. of calories', fontsize=10)
+plt.title('1921-1930')
+plt.bar(ps.index, ps.values)
+plt.show()
 
 class EvaluateMarks:
     def totalgrp1(num):
